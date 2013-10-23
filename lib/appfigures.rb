96301c044fbde76c1c4f73e125e7fe07f4fe19c4
williamlocke <williamlocke@me.com>
2013-10-23 00:05:52 -0400

    1st

---
+++ lib/appfigures.rb
@@ -0,0 +1,79 @@
+require 'appfigures/version'
+require 'appfigures/connection'
+
+require 'date'
+
+class Appfigures
+  attr_reader :connection
+  def initialize(options = {})
+    @connection = Appfigures::Connection.new options[:username], options[:password]
+  end
+
+  def product_sales
+    self.connection.get('sales/products').body.map do |id, hash|
+      Hashie::Mash.new({
+        'product_id'      => hash['product']['id'],
+        'store_id'        => hash['product']['store_id'],
+        'store_name'      => hash['product']['store_name'],
+        'name'            => hash['product']['name'],
+        'sku'             => hash['product']['sku'],
+        'downloads'       => hash['downloads'].to_i,
+        'returns'         => hash['returns'].to_i,
+        'updates'         => hash['updates'].to_i,
+        'net_downloads'   => hash['net_downloads'].to_i,
+        'promos'          => hash['promos'].to_i,
+        'gift_redemptions'=> hash['gift_redemptions'].to_i,
+        'revenue'         => hash['revenue'].to_f
+      })
+    end
+  end
+
+  def date_sales(start_date, end_date)
+    url = "sales/dates+products/#{start_date.strftime('%Y-%m-%d')}/#{end_date.strftime('%Y-%m-%d')}"
+    self.connection.get(url).body.map do |date, product|
+      product.map do |product_id, hash|
+        Hashie::Mash.new({
+          'date'            => Date.parse(date),
+          'product_id'      => hash['product']['id'],
+          'store_id'        => hash['product']['store_id'],
+          'store_name'      => hash['product']['store_name'],
+          'name'            => hash['product']['name'],
+          'sku'             => hash['product']['sku'],
+          'product_type'    => hash['product']['product_type'],
+          'parent_id'    => hash['product']['parent_id'],
+          'downloads'       => hash['downloads'].to_i,
+          'returns'         => hash['returns'].to_i,
+          'updates'         => hash['updates'].to_i,
+          'net_downloads'   => hash['net_downloads'].to_i,
+          'promos'          => hash['promos'].to_i,
+          'gift_redemptions'=> hash['gift_redemptions'].to_i,
+          'revenue'         => hash['revenue'].to_f
+        })
+      end
+    end
+  end
+  
+  
+  def products_search(term)
+    url = "/products/search/#{term}"
+    self.connection.get(url).body.map do |date, product|
+      product.map do |product_id, hash|
+        Hashie::Mash.new({
+          'public_product_id'      => hash['public_product_id'],
+          'refno'      => hash['refno'],
+          'sku'      => hash['sku'],
+          'name'      => hash['name'],
+          'developer'      => hash['developer'],
+          'icon'      => hash['icon'],
+          'price'      => hash['price']['value'],
+          'store'      => hash['store']
+        })
+      end
+    end
+  end
+  
+  
+  
+
+
+end
